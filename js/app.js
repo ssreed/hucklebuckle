fc3b13541ed085bfa44fb075ced408c9ec7dab67
Sri <srisreed@gmail.com>
2013-05-20 16:57:59 -0700

    removed init()

--- js/app.js
+++ js/app.js
@@ -7,28 +7,6 @@ var randomGen = function() {
     userGuess = document.getElementById("guess").value;
     userGuess = parseInt(userGuess);
     console.log("val is " +  userGuess);
-    console.log("rand is " +  randNum);
-    init();
+    console.log("rand is " +  randNum); 
 };
 
-var init = function() {
-    console.log("in init: " + randNum);
-    answer = document.getElementById('answer');
-    answer.innerHTML = "Your guess was " + userGuess;
-    console.log(typeof userGuess + " and " + typeof randNum);
-    console.log(userGuess === randNum);
-    while(userGuess !== randNum)
-    {   
-        if(userGuess === randNum)
-        {
-            alert('Nice!'); 
-        }
-        else
-        {
-            userGuess = null;
-            return;
-        }
-        console.log('here');
-    }
-    console.log('VAL IS : ' + userGuess);
-};
