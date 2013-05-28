9de6fa7ab9a168157e9eda3aa0e18f8c7788cada
Sri <srisreed@gmail.com>
2013-05-28 14:50:03 -0700

    guessing game works properly now

--- js/app.js
+++ js/app.js
@@ -1,12 +1,45 @@
-var userGuess = 0,
-    randNum = 0,
-    answer;
+(function(app, undefined){
+  //private variables
+  var userGuess;
+  var randNum;
+  var answer;
 
-var randomGen = function() {
+  //private function
+  randomGen = function(){
     randNum = Math.floor(100 * Math.random());
+    return randNum;
+  },
+
+  //public
+  app.init = function(){
+
+    var randomNum = randomGen();
+
     userGuess = document.getElementById("guess").value;
     userGuess = parseInt(userGuess);
-    console.log("val is " +  userGuess);
-    console.log("rand is " +  randNum); 
-};
 
+    console.log("User guess: " + userGuess);
+    console.log("Random val: " + randNum);
+
+    answer = document.getElementById('answer');
+    answer.innerHTML = "Your guess was " + userGuess;
+
+
+    var randomNumtoStr = " " + randomNum;
+    while(userGuess !== randomNum)
+    {
+
+      if(userGuess < randomNum) {
+        userGuess = Number(prompt('Your guess is too low'));
+      } else {
+        userGuess = Number(prompt('your guess is too high'));
+      }
+    }
+
+    alert('good job!');
+
+  }
+
+})(window.app = window.namespace || {});
+
+console.log(app);
